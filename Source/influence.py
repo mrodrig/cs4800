469e58769903f1fb3015eb1161a0c816c9bb8c9b
Mike Rodrigues <rodrigues.mi@husky.neu.edu>
2014-11-12 12:39:43 -0500

    pushing another attempt - still too slow. working on a better approach though.

--- Source/influence.py
+++ Source/influence.py
@@ -8,8 +8,10 @@ import sys
 bossIds = [0]
 # Array where values[0] = 0, values[1] = CEO's Value, values[<i>] = i's value
 values = [0]
+# Array of arrays where subordinates[1] is the list of subordinates
+subordinates = [[]]
 # Set max influence effect for each employee
-effect = []
+effects = [0]
 # Set of Ids of employees we have influenced
 influenced = []
 # Array of values for the optimal solution. Opt[num_employees+1] is optimal
@@ -26,9 +28,11 @@ def printUsage():
 # Adds the employee's information to the appropriate array
 # O(1) time
 def addEmployee(empId, bossId, value):
-	bossIds.append(int(bossId))
-	values.append(int(value))
-
+	subordinates.append([]) # Add this employee to the subordinates list
+	subordinates[bossId].append(empId) # Add this employee ID as subordinate to his/her boss
+	values.append(value)
+	effects.append(effects[bossId] + value)
+	bossIds.append(bossId)
 
 # Computes and returns the total influence value computed by the algorithm
 # O(n) time
@@ -44,6 +48,7 @@ def calculateInfluenceTotal():
 # to the 'influenced' set.  This prevents duplicates from occurring, ensuring correctness
 # O(m) time
 def influence(empId):
+	#print "Influencing empId: %d" % (empId)
 	while(not(empId == 0)):
 		influenced[empId] = True
 		empId = bossIds[empId]
@@ -55,11 +60,11 @@ def influence(empId):
 # O(m) time
 def employeeInfluenceEffect(empId):
 	acc = 0
-	while(not(empId == 0)):
-		acc += values[empId]
+	while (not(empId == 0)):
+		if (not influenced[empId]):
+			acc += values[empId]
 		empId = bossIds[empId]
 	return acc
-
 
 def findOpt(i, j): # the optimal solution for employees 1...i with up to j influences
 	global opt_values
@@ -85,7 +90,6 @@ def main():
 	num_to_influence = int(argv[argc-1])
 
 	# Intialize the influenced array to all False
-	effect = [0] * (num_employees+1)
 	influenced = [False] * (num_employees+1)
 	# The following line is causing the program to run extremely slow on large inputs:
 	#opt_values = [[0 for x in range(num_to_influence + 1)] for x in range(num_employees + 1)]
@@ -94,7 +98,7 @@ def main():
 	for i in range(num_employees):
 		line = sys.stdin.readline().rstrip()
 		vals = line.split(" ")
-		addEmployee(vals[0], vals[1], vals[2])
+		addEmployee(int(vals[0]), int(vals[1]), int(vals[2]))
 
 	# If we cannot influence anyone, then value is 0
 	if (num_to_influence == 0):
@@ -105,13 +109,49 @@ def main():
 		influenced[0] = False # Cannot influence a non-existent employee
 		print calculateInfluenceTotal()
 
+	# Find all employees that are at the lowest level (have no subordinates)
+	lowestLevelWorkerIds = []
+	maxEffect = -1
+	maxId     = -1
+	for i in range(1, num_employees+1):
+		if (subordinates[i] == []):
+			effect = employeeInfluenceEffect(i)
+			if effect > maxEffect:
+				maxEffect = effect
+				maxId = i
+			lowestLevelWorkerIds.append((i, effect))
+
+	if (len(lowestLevelWorkerIds) == 0):
+		print "There is an error with the data - hierarchy is circular"
+	else:
+		influence(maxId) # Influence the employee with the max value
+		lowestLevelWorkerIds.remove((maxId, maxEffect))
+		for i in range(1, num_to_influence):
+			maxEffect = -1
+			maxId     = -1
+			for i in range(len(lowestLevelWorkerIds)):
+				(empId, effect) = lowestLevelWorkerIds[i]
+				effect = employeeInfluenceEffect(empId)
+				lowestLevelWorkerIds[i] = (empId, effect)
+				if effect > maxEffect:
+					maxEffect = effect
+					maxId = empId
+			influence(maxId)
+			try:
+				lowestLevelWorkerIds.remove((maxId, maxEffect))
+			except ValueError:
+				print "ERROR", lowestLevelWorkerIds, (maxId, maxEffect)
+
+	print calculateInfluenceTotal()
+
+	# DYNAMIC PROGRAMMING ATTEMPT:
 	# Find optimal solutions
-	for i in range(0, num_employees+1):
-		opt_values.append([0] * (num_to_influence + 1))
-		for j in range(1, num_to_influence+1):
-			findOpt(i, j)
+	#for i in range(0, num_employees+1):
+	#	opt_values.append([0] * (num_to_influence + 1))
+	#	for j in range(1, num_to_influence+1):
+	#		#findOpt(i, j)
+	#		print "i: %d\t j: %d" % (i, j)
 
-	print opt_values[num_employees][num_to_influence]
-
+	#print opt_values[num_employees][num_to_influence]
 
 main()
