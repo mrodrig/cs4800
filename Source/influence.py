de397da5bd2be6dd819be6928f01d5d1a9f2d74c
Mike Rodrigues <rodrigues.mi@husky.neu.edu>
2014-11-16 00:44:46 -0500

    efficient algorithm, working on fixing walk back up the tree

--- Source/influence.py
+++ Source/influence.py
@@ -18,16 +18,12 @@ influenced = []
 residualEffect = []
 # List of tuples of scanned employees
 scannedEmployees = []
-# Is this employee a max of a branch?
-isMax = []
-
 
 # Prints the correct script usage and exits
 def printUsage():
 	print "Error: Invalid number of arguments"
 	print "  USAGE: python influence.py <number_of_employees> <number_to_influence>"
 	sys.exit(1)
-
 
 # Adds the employee's information to the appropriate array
 # O(1) time
@@ -38,44 +34,19 @@ def addEmployee(empId, bossId, value):
 	effects.append(effects[bossId] + value)
 	bossIds.append(bossId)
 
-# Computes and returns the total influence value computed by the algorithm
-# O(n) time
-def calculateInfluenceTotal():
-	acc = 0
-	for empId in range(len(influenced)):
-		if influenced[empId]: # If this employee was influenced
-			acc += values[empId]
-	return acc
-
-
-# Given an employee id, this function adds the ids of all employees that would be influenced
-# to the 'influenced' set.  This prevents duplicates from occurring, ensuring correctness
-# O(m) time
-def influence(empId):
-	#print "Influencing empId: %d" % (empId)
-	while(not(empId == 0)):
-		influenced[empId] = True
-		empId = bossIds[empId]
-
-
-# Returns the total influence effect that influencing the given employee would have
-# This function follows the bossID pointers to calculate the total effect back to the CEO
-# Returns a non-negative number
-# O(m) time
-def employeeInfluenceEffect(empId):
-	acc = 0
-	while (not(empId == 0)):
-		if (not influenced[empId]):
-			acc += values[empId]
-		empId = bossIds[empId]
-	return acc
+# Resets the influenced value to False for employee with id = id and all subordinates
+def reset(id):
+	influenced[id] = False
+	for sub in subordinates[id]:
+		reset(sub)
 
 # id: current employee id, value: boss' value, maxVal: maximum value seen so far
 def scanTree(id, value, max):
-	if id == 15:
-		print value, max
-	if (not isMax[id]):
+	print (id, value, max)
+	if (value + values[id] > max):
 		residualEffect[id] = value + values[id]
+	else:
+		residualEffect[id] = values[id]
 
 	# List of subordinates
 	subList = subordinates[id]
@@ -86,20 +57,22 @@ def scanTree(id, value, max):
 
 	branchMax = residualEffect[id]
 	branchMaxId = id
+	print [effects[id], residualEffect[id]]
 	for subordinate in subList:
 		(subId, subEffect, subBranchMax) = scanTree(subordinate, 0 if influenced[id] else effects[id], branchMax)
 		if subBranchMax > branchMax:
 			branchMax = subBranchMax
 			branchMaxId = subId
-	isMax[branchMaxId] = True
-	for subordinate in subList: # Update non chosen employees, starting with 0 for value
-		(subId, subEffect, subBranchMax) = scanTree(subordinate, 0, branchMax)
+	branchMaxIndex = subList.index(branchMaxId) if branchMaxId in subList else 0
+
 	residualEffect[id] = 0 # no point influencing a non leaf
-	influenced[id] = True
+	for subordinate in subList[0:branchMaxIndex]: # Update non max influence employees, starting with 0 for value
+		print str(id) + " fixing sub via " + str((subordinate, residualEffect[id], branchMax))
+		(subId, subEffect, subBranchMax) = scanTree(subordinate, residualEffect[id], branchMax)
 	return (id, residualEffect[id], branchMax)
 
 def main():
-	global influenced, residualEffect, isMax
+	global influenced, residualEffect
 	argv = sys.stdin.readline().strip().split(" ")
 	argc = len(argv)
 	if (not(argc == 2)):
@@ -111,7 +84,6 @@ def main():
 
 	# Intialize the influenced array to all False
 	influenced = [False] * (num_employees+1)
-	isMax = [False] * (num_employees+1)
 	residualEffect = [0] * (num_employees+1)
 
 	# Fetch each employee's information
@@ -136,8 +108,8 @@ def main():
 	scannedEmployees.sort(key=lambda tup: tup[1], reverse=True) # Decreasing order
 	print scannedEmployees
 	result = 0
-	for i in range(num_to_influence):
-		result += scannedEmployees[i][1] # Add the result
+	for (id, effect) in scannedEmployees[0:num_to_influence]:
+		result += effect # Add the result
 	print result
 
 main()
